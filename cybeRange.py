b5c7500d9169dfa1a8e90b3bcd1db5dc5ded1b45
micahjmartin <mjm5097@rit.edu>
2019-05-02 19:47:36 -0400

    adding basic homepage andfunctionality

--- cybeRange.py
+++ cybeRange.py
@@ -1,12 +1,22 @@
 from aiohttp import web
+import aiohttp_jinja2
 from cybeRange import app, routes
 
 
 @routes.get('/')
+@aiohttp_jinja2.template('index.html')
 async def hello(request):
-    return web.Response(text="Hello, world")
+    return {
+        "types": list(app['config'].get("mal_types", []))
+    }
+
+@routes.get('/upload')
+async def get_upload(request):
+    data = await request.multipart()
+
 
 
 if __name__ == '__main__':
-    app.add_routes(routes)    
+    print(app.get('config'))
+    app.add_routes(routes)
     web.run_app(app)
--- cybeRange/__init__.py
+++ cybeRange/__init__.py
@@ -1,4 +1,19 @@
 from aiohttp import web
+import aiohttp_jinja2
+import yaml
+import jinja2
+import pathlib
 
 routes = web.RouteTableDef()
 app = web.Application()
+aiohttp_jinja2.setup(app, loader=jinja2.FileSystemLoader('cybeRange/templates'))
+
+routes.static('/static', 'cybeRange/static')
+
+def get_config():
+    filename = pathlib.Path(__file__).parent / "config.yml"
+    with open(filename) as fil:
+        config = yaml.load(fil)
+    return config
+
+app['config'] = get_config()
