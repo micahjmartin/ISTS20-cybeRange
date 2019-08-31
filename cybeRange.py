a07e498822b65d7fe03366e3acc19b64bb575447
Micah J. Martin <mjm5097@rit.edu>
2019-08-31 16:09:49 -0400

    update readme and the code

--- cybeRange.py
+++ cybeRange.py
@@ -7,22 +7,77 @@ from asyncio import create_task
 import aiohttp_jinja2
 from cybeRange import app, routes
 import hashlib
-from cybeRange.worker import getBase64Strings, getStrings, execute
+
+import base64
+from binascii import Error
+import re
+import subprocess
+
+
+def getStrings(filename):
+    """
+    Get the 'strings' output of the data. Only show strings longer than 6
+    """
+    p = subprocess.Popen(
+        ["strings", filename, "-n", "6"], stdout=subprocess.PIPE, text=True
+    )
+    out, _ = p.communicate()
+    strings = out.strip()
+    return strings
+
+
+def getBase64Strings(strings):
+    """
+    Search the given text for Base64 strings, if they are detected, decode them
+    """
+    if not strings:
+        return ""
+    # Find all the base64 strings in the data
+    reg = re.compile(r"[A-Za-z0-9+/]{7,}=*")
+    # for string in strings:
+    base64strings = list(reg.findall(strings))
+
+    valid = []
+    for string in base64strings:
+        try:
+            valid += [base64.standard_b64decode(string).decode()]
+        except (UnicodeDecodeError, Error):
+            pass
+    return "\n".join(valid)
+
+
+def execute(args, timeout=None):
+    """
+    Execute a program and return information after execution
+    """
+    retval = {"status": 255, "stdout": "", "stderr": ""}
+    try:
+        proc = subprocess.Popen(
+            args, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True
+        )
+        retval["stdout"], retval["stderr"] = proc.communicate(timeout=30)
+        retval["status"] = proc.wait()
+        retval["message"] = "The process executed normally"
+    except subprocess.TimeoutExpired:
+        retval["message"] = "The process exceeded the time limit (30 seconds)"
+    except UnicodeDecodeError:
+        retval["message"] = "Binary output detected in program"
+    return retval
 
 
 async def analyze(filename, filetype, jobid):
+    """Async function that will analyze the program and save the results in a json template.
+    When /results/JOBID is called, the results file will be read
+    """
+    output = {"type": filetype, "filename": filename, "jobid": jobid, "antiv": []}
+
     filename = "jobs/{}/{}".format(jobid, filename)
     command = app["config"].get("mal_types", {}).get(filetype, None)
     command = command.format(filename)
+    output["command"] = command
 
-    output = {
-        "command": command,
-        "type": filetype,
-        "filename": filename,
-        "jobid": jobid,
-        "strings": getStrings(filename),
-        "antiv": [],
-    }
+    # Run strings and the program
+    output["strings"] = getStrings(filename)
     output["b64_strings"] = getBase64Strings(output["strings"])
     retval = execute(command)
     output.update(retval)
@@ -52,7 +107,6 @@ async def get_upload(request):
 
     # Get the type of the malware
     field = await data.next()
-    # assert field.name == 'type'
     typ = await field.read()  # decode=True)
 
     # Get the file of the malware
@@ -62,16 +116,18 @@ async def get_upload(request):
 
     size = 0
 
-    # Generate a jobid
+    # Generate a "random" jobid
     hsh = hashlib.md5()
     hsh.update(filename.encode("utf-8"))
     hsh.update(int(time.time()).to_bytes(8, byteorder="big"))
     jobid = hsh.hexdigest()
-    outdir = "jobs/" + jobid
+
     # Create the directory we are saving the file in
+    outdir = "jobs/" + jobid
     if not os.path.exists(outdir):
         os.makedirs(outdir)
 
+    # Read the file chunk by chunk and save it on disk
     with open("{}/{}".format(outdir, filename), "wb") as outfile:
         while True:
             chunk = await field.read_chunk()
@@ -80,9 +136,9 @@ async def get_upload(request):
             size += len(chunk)
             outfile.write(chunk)
 
-    # Get the command to execute for that type
     typ = typ.decode("utf-8")
 
+    # Spawn the async function that runs the binary
     create_task(analyze(filename, typ, jobid))
 
     return {"filename": filename, "size": size, "job": jobid}
@@ -92,13 +148,11 @@ async def get_upload(request):
 @aiohttp_jinja2.template("results.html")
 async def results(request):
     jobid = request.match_info["jobid"]
-    # Save the results
     filename = "jobs/{}/results.json".format(jobid)
     if not os.path.exists(filename):
         return {"message": "Invalid Job", "error": True}
     with open(filename) as fil:
         data = json.load(fil)
-
     return data
 
 
--- test.py
+++ test.py
@@ -10,7 +10,21 @@ import base64
 import time
 import re
 
-mal_names = ["l33t_warez.sh", "dank_shellz", "meterpreter", "code.txt", "test.sh", "ls", "whoami", "a.sh", "a", "tmp", "v2", "v6"]
+mal_names = [
+    "l33t_warez.sh",
+    "dank_shellz",
+    "meterpreter",
+    "code.txt",
+    "test.sh",
+    "ls",
+    "whoami",
+    "a.sh",
+    "a",
+    "tmp",
+    "v2",
+    "v6",
+]
+
 
 def generate_script(type="bash"):
     """This function generates a script that should print out 3 different flags"""
@@ -24,17 +38,27 @@ def generate_script(type="bash"):
     echo START;
     cat flagfile;
     ls -d /*;
+    # {flag4}
     echo "user = `whoami`"
     """
 
-    flag1 = ''.join(random.choice(string.ascii_letters) for i in range(48))
-    flag2 = ''.join(random.choice(string.ascii_letters) for i in range(48))
+    flag1 = "".join(random.choice(string.ascii_letters) for i in range(48))
+
+    # This flag should only exist if the program decodes the string
+    flag2 = "".join(random.choice(string.ascii_letters) for i in range(48))
     flag2_b64 = base64.standard_b64encode(flag2.encode("utf-8")).decode()
-    flag3 = ''.join(random.choice(string.ascii_letters) for i in range(48))
 
-    retval = retval.format(flag=flag1, flag2=flag2_b64, flag3=flag3)
+    # This flag should be printed out after START proving that we can read/write to files
+    flag3 = "".join(random.choice(string.ascii_letters) for i in range(48))
 
-    return retval, (flag1, flag2, "START\n.*{}".format(flag3), "/bin")
+    # This flag just sits in the program and should be decoded by the analysis portion instead of during runtime
+    flag4 = "".join(random.choice(string.ascii_letters) for i in range(12))
+    flag4_b64 = base64.standard_b64encode(flag4.encode("utf-8")).decode()
+
+    retval = retval.format(flag=flag1, flag2=flag2_b64, flag3=flag3, flag4=flag4_b64)
+
+    return retval, (flag1, flag2, "START\n.*{}".format(flag3), "/bin", flag4)
+
 
 def test(server):
     """This function will generate a script and have the server run it"""
@@ -42,27 +66,31 @@ def test(server):
     filename = random.choice(mal_names)
     script, flags = generate_script()
     multipart_form_data = {
-        'type': ('', "bash"),
-        'warez': (filename, script.encode('utf-8')),
+        "type": ("", "bash"),
+        "warez": (filename, script.encode("utf-8")),
     }
     # Send the script to the server
-    response = requests.post('{}/upload'.format(server),
-                             files=multipart_form_data)
-    
+    response = requests.post("{}/upload".format(server), files=multipart_form_data)
+
     if response:
         content = response.content.decode("utf-8")
-    
+
     if response.status_code != 200:
-        print("FAIL: Submission of the script failed. Code:", response.status_code, "Content:", content)
+        print(
+            "FAIL: Submission of the script failed. Code:",
+            response.status_code,
+            "Content:",
+            content,
+        )
         return
-    
+
     # Assume it worked, Get the job ID from the response
     jobid = re.search(r"href=\"/results/([\d[a-z]+)\"", content)
 
     if not jobid:
         print("FAIL: No JOBID detected in response. Content:", content)
         return
-    
+
     jobid = jobid.group(1)
 
     # Wait a few seconds for the script to run
@@ -75,9 +103,11 @@ def test(server):
 
     if resp:
         content = resp.content.decode("utf-8")
-    
+
     if resp.status_code != 200:
-        print("FAIL: Getting results failed. Code:", resp.status_code, "Content:", content)
+        print(
+            "FAIL: Getting results failed. Code:", resp.status_code, "Content:", content
+        )
         return
 
     # Search through the content for the flags
