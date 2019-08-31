ad45f7435b9ce637c563c8f3e454e14e141dbd36
Micah J. Martin <mjm5097@rit.edu>
2019-08-31 14:32:03 -0400

    Added fake-av results

--- cybeRange.py
+++ cybeRange.py
@@ -1,6 +1,42 @@
+import os
+import time
+import json
+import random
 from aiohttp import web
+from asyncio import create_task
 import aiohttp_jinja2
 from cybeRange import app, routes
+import hashlib
+from cybeRange.worker import getBase64Strings, getStrings, execute
+
+
+async def analyze(filename, filetype, jobid):
+    filename = "jobs/{}/{}".format(jobid, filename)
+    command = app["config"].get("mal_types", {}).get(filetype, None)
+    command = command.format(filename)
+
+    output = {
+        "command": command,
+        "type": filetype,
+        "filename": filename,
+        "jobid": jobid,
+        "strings": getStrings(filename),
+        "antiv": [],
+    }
+    output["b64_strings"] = getBase64Strings(output["strings"])
+    retval = execute(command)
+    output.update(retval)
+
+    # Generate fake Anti-virus reports
+    for i in ["Clam AV", "Norton", "Kapersky", "MalwareBytes", "McAffee"]:
+        if random.choice([True, True, True, True, False]):
+            output["antiv"] += [{"av": i, "img": "check.png"}]
+        else:
+            output["antiv"] += [{"av": i, "img": "x.png"}]
+
+    # Save the results
+    with open("jobs/{}/results.json".format(jobid), "w") as ofil:
+        ofil.write(json.dumps(output))
 
 
 @routes.get("/")
@@ -25,7 +61,18 @@ async def get_upload(request):
     filename = field.filename
 
     size = 0
-    with open("/tmp/{}".format(filename), "wb") as outfile:
+
+    # Generate a jobid
+    hsh = hashlib.md5()
+    hsh.update(filename.encode("utf-8"))
+    hsh.update(int(time.time()).to_bytes(8, byteorder="big"))
+    jobid = hsh.hexdigest()
+    outdir = "jobs/" + jobid
+    # Create the directory we are saving the file in
+    if not os.path.exists(outdir):
+        os.makedirs(outdir)
+
+    with open("{}/{}".format(outdir, filename), "wb") as outfile:
         while True:
             chunk = await field.read_chunk()
             if not chunk:
@@ -35,16 +82,29 @@ async def get_upload(request):
 
     # Get the command to execute for that type
     typ = typ.decode("utf-8")
-    print(typ)
-    command = app["config"].get("mal_types", {}).get(typ, None)
-    assert command != None
-    print(command.format(filename))
-    # TODO: Random Job name here
-    # TODO: Actually call the action whenever it works
-    return {"filename": filename, "size": size, "job": "asdlfhk4r8fadf"}
+
+    create_task(analyze(filename, typ, jobid))
+
+    return {"filename": filename, "size": size, "job": jobid}
+
+
+@routes.get("/results/{jobid:[\da-z]+}")
+@aiohttp_jinja2.template("results.html")
+async def results(request):
+    jobid = request.match_info["jobid"]
+    # Save the results
+    filename = "jobs/{}/results.json".format(jobid)
+    if not os.path.exists(filename):
+        return {"message": "Invalid Job", "error": True}
+    with open(filename) as fil:
+        data = json.load(fil)
+
+    return data
 
 
 if __name__ == "__main__":
     print(app.get("config"))
+    if not os.path.exists("jobs"):
+        os.makedirs("jobs")
     app.add_routes(routes)
     web.run_app(app)
--- cybeRange/worker.py
+++ cybeRange/worker.py
@@ -8,27 +8,30 @@ import subprocess
 def getStrings(filename):
     """Get the 'strings' output of the data. Only show longer strings or ones that are interesting
     """
-    p = subprocess.Popen(["strings", filename, "-n", "6"], stdout=subprocess.PIPE)
+    p = subprocess.Popen(
+        ["strings", filename, "-n", "6"], stdout=subprocess.PIPE, text=True
+    )
     out, _ = p.communicate()
-    strings = out.decode("utf-8").strip().split("\n")
+    strings = out.strip()
     return strings
 
 
 def getBase64Strings(strings):
     if not strings:
-        return []
-    base64strings = []
+        return ""
     # Find all the base64 strings in the data
     reg = re.compile(r"[A-Za-z0-9+/]{7,}=*")
-    for string in strings:
-        base64strings += reg.findall(string)
+    # for string in strings:
+    base64strings = list(reg.findall(strings))
+    # base64strings = []
+
     valid = []
     for string in base64strings:
         try:
             valid += [base64.standard_b64decode(string).decode()]
         except (UnicodeDecodeError, Error):
             pass
-    return valid
+    return "".join(valid)
 
 
 def execute(args, timeout=None):
@@ -38,13 +41,12 @@ def execute(args, timeout=None):
     retval = {"status": 255, "stdout": "", "stderr": ""}
     try:
         proc = subprocess.Popen(
-            args, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE
+            args, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True
         )
         retval["stdout"], retval["stderr"] = proc.communicate(timeout=30)
         retval["status"] = proc.wait()
         retval["message"] = "The process executed normally"
-    except Exception as E:
-        print(type(E), E)
+    except subprocess.TimeoutExpired:
         retval["message"] = "The process exceeded the time limit (30 seconds)"
     return retval
 
