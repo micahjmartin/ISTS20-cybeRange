b0e427ce5051d563db8a5c8aa576eb8f72b639b6
Micah J. Martin <mjm5097@rit.edu>
2019-08-31 12:34:14 -0400

    Updated worker to get execution running

--- cybeRange/worker.py
+++ cybeRange/worker.py
@@ -1,35 +1,32 @@
 import sys
 import base64
 from binascii import Error
-import asyncio
 import re
+import subprocess
 
 def getStrings(filename):
     """Get the 'strings' output of the data. Only show longer strings or ones that are interesting
     """
-    cmd = "strings {}".format(filename)
-    proc = await asyncio.create_subprocess_shell(
-        cmd,
-        stdout=asyncio.subprocess.PIPE
-    )
-
-    # Sort only the strings longer than 6 chars
-    strings = [x for x in strings if len(x) > 6]
+    p = subprocess.Popen(["strings", filename, "-n", "6"], stdout=subprocess.PIPE)
+    out, _ = p.communicate()
+    strings = out.decode("utf-8").strip().split("\n")
     return strings
 
 def getBase64Strings(strings):
     if not strings:
         return []
+    base64strings = []
     # Find all the base64 strings in the data
-    reg = re.compile(r"^[A-Za-z0-9+/]+=+$")
-    base64strings = list(filter(reg.match, strings))
+    reg = re.compile(r"[A-Za-z0-9+/]{7,}=*")
+    for string in strings:
+        base64strings += reg.findall(string)
     valid = []
     for string in base64strings:
         try:
             valid += [base64.standard_b64decode(string).decode()]
-        except (UnicodeDecodeError, Error) as E:
+        except (UnicodeDecodeError, Error):
             pass
-    print(valid)
+    return valid
 
 def execute(args, timeout=None):
     '''
@@ -37,21 +34,25 @@ def execute(args, timeout=None):
     '''
     retval = {'status': 255}
     try:
-        proc = Popen(args, shell=True, stdout=PIPE, stderr=PIPE, stdin=PIPE,
-                     close_fds=True)
-        retval['stdout'] = proc.stdout.read().decode("utf-8")
-        retval['stderr'] = proc.stderr.read().decode("utf-8")
-        retval['status'] = proc.wait(timeout=timeout)
+        proc = subprocess.Popen(args, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        retval['stdout'], retval['stderr'] = proc.communicate(timeout=30)
+        retval['status']  = proc.wait()
+        retval['message'] = "The process executed normally"
     except Exception as E:
-        print(args)
-        print(E)
+        print(type(E), E)
+        retval['message'] = "The process exceeded the time limit (30 seconds)"
     return retval
 
 def main():
     # Get the strings of the file
     strs = getStrings(sys.argv[1])
     decoded_strs = getBase64Strings(strs)
+
+    print("Detected strings:", strs)
+    print("Decoded B64 strings:", decoded_strs)
     # Actually run the file
+    print(execute("bash {}".format(sys.argv[1])))
+
 
 
 if __name__ == "__main__":
